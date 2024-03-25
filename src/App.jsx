70ab22422cbd45ac49a817f06147373096a03ae5
Abdullah001butt <anasbutt2014@gmail.com>
2024-03-25 00:10:58 +0000

    10 commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const [showLogin,setShowLogin] = useState(false)
 
   return (
     <>
-    {showLogin?<LoginPopup/>:<></>}
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className="app">
         <Navbar setShowLogin={setShowLogin}/>
         <Routes>
