ae1ca207d1bb7cfec85d1f1849f7cf2cc82ec3f5
Abdullah001butt <anasbutt2014@gmail.com>
2024-03-25 15:50:48 +0000

    11 commit

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import "./Cart.css"
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
-export default Cart
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={index} className="cart-items-title cart-items-item">
+                <img src={item.image} alt="" />
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
