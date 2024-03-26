34f2cb03e06bb0409073f3e83cd1ccf33d03cb06
Abdullah001butt <anasbutt2014@gmail.com>
2024-03-26 17:40:28 +0000

    13 commit

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -21,17 +21,52 @@ const Cart = () => {
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div key={index} className="cart-items-title cart-items-item">
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>{item.price*cartItems[item._id]}</p>
-                <p>X</p>
+              <div key={index}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
               </div>
             );
           }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fees</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+          </div>
+          <button>Proceed to Checkout</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>if you have promo code. Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder="Enter promo code"/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
